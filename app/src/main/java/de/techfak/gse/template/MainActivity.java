ff5942347dacb7644e8ed560c39555d66b0c1e48
root <root@se.techfak.de>
2021-10-16 16:04:02 +0200

    Add default project structure

---
+++ app/src/main/java/de/techfak/gse/template/MainActivity.java
@@ -0,0 +1,17 @@
+package de.techfak.gse.template;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Entry point activity for the app.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
