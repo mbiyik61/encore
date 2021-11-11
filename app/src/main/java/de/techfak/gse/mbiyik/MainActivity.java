050e51d9997afcdd413437b01d44139fdfdbeb6f
mbiyik <mbiyik@techfak.de>
2021-11-11 21:45:31 +0100

    Add EditText.Add Log message containing the config. closes #34593, closes #34486 @1.5h

--- app/src/main/java/de/techfak/gse/mbiyik/MainActivity.java
+++ app/src/main/java/de/techfak/gse/mbiyik/MainActivity.java
@@ -3,6 +3,10 @@ package de.techfak.gse.mbiyik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Entry point activity for the app.
@@ -13,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClick(View view) {
+        EditText editText = findViewById(R.id.config);
+
+        Log.i("log",String.valueOf(editText.getText()));
     }
 }
