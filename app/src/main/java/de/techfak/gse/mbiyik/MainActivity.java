e00da1c4c1c72f3fbfdcf1fccb6cc8a74d1d1da7
mbiyik <mbiyik@techfak.de>
2021-11-13 15:17:56 +0100

    Add exception which shows a warning if config is not valid.The user may try again. closes #34832 @0.5h

--- app/src/main/java/de/techfak/gse/mbiyik/MainActivity.java
+++ app/src/main/java/de/techfak/gse/mbiyik/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+
 
 /**
  * Entry point activity for the app.
@@ -23,6 +26,35 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         EditText editText = findViewById(R.id.config);
 
-        Log.i("log",String.valueOf(editText.getText()));
+        Log.i("log", String.valueOf(editText.getText()));
+        String input = editText.getText().toString();
+        String[] configs = input.split("\n");
+        if (!(valideLayout(configs,view))) {
+            editText.setText("");
+            editText.setHint("Bitte versuchen Sie es erneut.");
+
+        }
+
     }
+    public boolean valideLayout(String[] input,View view) {
+        int count = 0;
+        if (input.length==7) {
+            for (int i=0;i<7;i++) {
+                if (input[i].length()==15) {
+                    count++;
+                }
+            }
+        }
+        try {
+            if (count != 7) {
+                throw new Exception("InvalidBoardLayout");
+            }
+
+        } catch (Exception e) {
+            Snackbar.make(view,"Das eingegebene Spielfeld ist nicht gültig!", Snackbar.LENGTH_SHORT).show();
+            Log.e("InvalidBoardLayout", "Die Eingabe für die Spielfeldkonfiguration ist größer oder kleiner als 7x15");
+        }
+        return count == 7;
+    }
+
 }
