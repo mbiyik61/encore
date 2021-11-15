8011580164438842674303c842e0d3e97080b7f6
mbiyik <mbiyik@techfak.de>
2021-11-15 15:20:43 +0100

    Add function valideField checking the input for invalid characters and if there are any, send error to UI via InvalidField Exception. closes #35084 @1.5h

--- app/src/main/java/de/techfak/gse/mbiyik/MainActivity.java
+++ app/src/main/java/de/techfak/gse/mbiyik/MainActivity.java
@@ -28,33 +28,67 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("log", String.valueOf(editText.getText()));
         String input = editText.getText().toString();
-        String[] configs = input.split("\n");
-        if (!(valideLayout(configs,view))) {
+        if ((!valideLayout(input,view,7,15)) || (!valideField(input, view))) {
             editText.setText("");
             editText.setHint("Bitte versuchen Sie es erneut.");
 
         }
 
+
+
     }
-    public boolean valideLayout(String[] input,View view) {
+
+    public boolean valideLayout(String input,View view, int zeilen,int spalten) {
         int count = 0;
-        if (input.length==7) {
-            for (int i=0;i<7;i++) {
-                if (input[i].length()==15) {
+        String[] configs = input.split("\n");
+        if (configs.length==zeilen) {
+            for (int i=0;i<zeilen;i++) {
+                if (configs[i].length()==spalten) {
                     count++;
                 }
             }
         }
         try {
-            if (count != 7) {
+            if (count != zeilen) {
                 throw new Exception("InvalidBoardLayout");
             }
 
         } catch (Exception e) {
-            Snackbar.make(view,"Das eingegebene Spielfeld ist nicht gültig!", Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(view,"Das eingegebene Spielfeld ist nicht gültig!", Snackbar.LENGTH_LONG).show();
             Log.e("InvalidBoardLayout", "Die Eingabe für die Spielfeldkonfiguration ist größer oder kleiner als 7x15");
         }
-        return count == 7;
+        return count == zeilen;
+    }
+
+    public boolean valideField(String input, View view) {
+        String[] colors = {"b", "B", "g", "G", "o", "O", "r", "R", "y", "Y"};
+        String[] seperate = input.split("");
+        boolean contains = true;
+
+        try {
+            loop:
+            for (int i = 1; i < seperate.length; i++) {
+                if (!seperate[i].equals("\n") && !seperate[i].equals(" ")) {
+                    for (String color : colors) {
+                        if (seperate[i].matches(color)) {
+                            break;
+                        } else if ((!seperate[i].matches(color) && (color.equals("Y")))) {
+                            contains = false;
+                            break loop;
+
+                        }
+
+                    }
+                }
+            }
+            if (!contains){
+                throw new Exception("InvalidField");
+            }
+        } catch (Exception e) {
+            Snackbar.make(view,"Das eingegebene Spielfeld enthält mindestens ein ungültiges Zeichen!", Snackbar.LENGTH_LONG).show();
+            Log.e("InvalidField", "Die Eingabe für das Spielfeld enthält mindestens ein ungültiges Zeichen.");
+        }
+        return contains;
     }
 
 }
